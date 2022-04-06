85d9ea449869dd251ed56f80d32d8ba1751d739b
ksdevinney <katie@devinney.org>
2022-04-05 21:29:30 -0500

    alternate methods for duplicate keys

--- hashtable.js
+++ hashtable.js
@@ -1,42 +1,68 @@
 class HashTable {
-    constructor() {
-        this.table = new Array(128);
-        this.size = 0;
-    }
+  constructor() {
+    this.table = new Array(128);
+    this.size = 0;
+  }
 
-    // create the hash value
-    // accepts a key and transforms it into an index
-    _hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash += key.CharCodeAt(i);
+  // create the hash value
+  // accepts a key and transforms it into an index
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.CharCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
+
+  // set a key/value pair in the table
+  set(key, value) {
+    const index = this._hash(key);
+
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index]; i++) {
+        // find key/value pair
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
         }
-        return hash % this.table.length;
+      }
+      // index not found, push key/value pair
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
     }
+    this.table[index] = [key, value];
 
-    // set a key/value pair in the table
-    set(key, value) {
-        const index = this._hash(key);
-        this.table[index] = [key, value];
-        this.size++;
-    }
+    this.size++;
+  }
 
-    // get a value from the table
-    get(key) {
-        const index = this._hash(key);
-        return this.table[index];
-    }
-
-    // remove a key/value pair from the table
-    remove(key) {
-        const index = this._hash(key);
-
-        if (this.table[index] && this.table[index].length) {
-            this.table[index] = undefined;
-            this.size--;
-            return true;
-        } else {
-            return false;
+  // get a value from the table
+  get(key) {
+    const target = this._hash(key);
+    if (this.table[target]) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[target][i][0] === key) {
+          return this.table[target][i][1];
         }
+      }
     }
+    return undefined;
+  }
+
+  // remove a key/value pair from the table
+  remove(key) {
+    const index = this._hash(key);
+
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table[index]; i++) {
+        if (this.table[index] === key) {
+          this.table.index.splice(i, 1);
+          this.size--;
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
 }
